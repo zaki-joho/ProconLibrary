de8e51c006a308bc0ed618ee8771b8764b995fb9
zaki-joho <zaki.joho.ku@gmail.com>
2019-02-12 21:51:57 +0900

    add closest pair

---
+++ Others/closest_pair.hpp
@@ -0,0 +1,43 @@
+// N 頂点の最近点対距離を O(NlogN) で求める
+// verified: http://judge.u-aizu.ac.jp/onlinejudge/review.jsp?rid=3380540
+using ld = long double;
+using P = pair<ld, ld>;
+const ld INF = 1e15;
+
+constexpr int MAX_N = 100000;
+int N;
+P A[MAX_N];
+
+bool compareY(P a, P b)
+{
+    return a.second < b.second;
+}
+
+ld closestPair(P *a, int n)
+{
+    if (n <= 1)
+        return INF;
+    int m = n / 2;
+    ld x = a[m].first;
+    ld d = min(closestPair(a, m), closestPair(a + m, n - m));
+    inplace_merge(a, a + m, a + n, compareY);
+    // a は y の昇順
+
+    vector<P> b;
+    for (int i = 0; i < n; i++)
+    {
+        if (fabs(a[i].first - x) >= d)
+            continue;
+
+        for (int j = 0; j < int(b.size()); j++)
+        {
+            ld dx = a[i].first - b[int(b.size()) - j - 1].first;
+            ld dy = a[i].second - b[int(b.size()) - j - 1].second;
+            if (dy >= d)
+                break;
+            d = min(d, sqrt(dx * dx + dy * dy));
+        }
+        b.push_back(a[i]);
+    }
+    return d;
+}
