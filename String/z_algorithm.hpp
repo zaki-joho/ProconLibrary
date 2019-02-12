49edb1269f69e2c6fa8fc50c756e0767c99a43ee
zaki-joho <zaki.joho.ku@gmail.com>
2019-02-12 21:55:11 +0900

    add z-algo

---
+++ String/z_algorithm.hpp
@@ -0,0 +1,33 @@
+// 文字列S の各i に対し, S と S[i:|S|-1] の最長共通接頭辞を求める
+// https://snuke.hatenablog.com/entry/2014/12/03/214243
+// verified: https://atcoder.jp/contests/arc055/submissions/2711907
+/*
+ * ex.  s = "aaabaaaab"
+ * -> z[i]= "*21034210"
+ */
+vector<int> z_algorithm(const string &s)
+{
+    int sz = s.length();
+    vector<int> z(sz, 0);
+    int i = 1, j = 0;
+    while (i < sz)
+    {
+        while (i + j < sz && s[j] == s[i + j])
+            j++;
+        z[i] = j;
+        if (j == 0)
+        {
+            i++;
+            continue;
+        }
+        int k = 1;
+        while (i + k < sz && k + z[k] < j)
+        {
+            z[i + k] = z[k];
+            k++;
+        }
+        i += k;
+        j -= k;
+    }
+    return z;
+}
