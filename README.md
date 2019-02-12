a22ed442ccdbabe0bda3d7a6cfc1f28c04f40d21
zaki-joho <zaki.joho.ku@gmail.com>
2019-02-12 21:07:23 +0900

    add template

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # ProconLibrary
+
+## –½–¼‹K‘¥
+
+ClassName
+methodName, functionName, variableName
+file_name, variable_name
+CONSTANT_NAME, ENUM_NAME, MACRO_NAME
---
+++ template.cpp
@@ -0,0 +1,57 @@
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using P = pair<int, int>;
+constexpr ld EPS = 1e-12;
+constexpr int INF = numeric_limits<int>::max() / 2;
+constexpr int MOD = 1e9 + 7;
+
+template <typename T>
+void printv(const vector<T> &v)
+{
+    int sz = v.size();
+    for (int i = 0; i < sz; i++)
+    {
+        cout << v[i] << " \n"[i == sz - 1];
+    }
+}
+
+int main()
+{
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+}
