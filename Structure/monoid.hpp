87dd616e97b006581c29e1000ef022137c162b7f
zaki-joho <zaki.joho.ku@gmail.com>
2019-02-16 18:24:35 +0900

    add RARS Monoid

--- Structure/monoid.hpp
+++ Structure/monoid.hpp
@@ -1,4 +1,5 @@
 #include "../bits/stdc++.h"
+// SegmentTree に載せるモノイド
 struct RangeMinimumQuery
 {
     using type = int;
@@ -19,3 +20,43 @@ struct RangeSumQuery
     static type id() { return 0; }
     static type op(const type &l, const type &r) { return l + r; }
 };
+
+// LazySegmentTree に載せるモノイド(未実装)
+// http://kazuma8128.hatenablog.com/entry/2017/12/29/081929
+/**
+ * テンプレート
+ * 操作1: 区間に対し, 含まれる全ての値をある演算で連結 (e.g. min, sum...)
+ * 操作2: 区間に対し, 含まれる全ての値をある値と作用させ置換 (e.g. update, add)
+struct QueryName
+{
+    using t1 = (保持データ, 操作1 の型);
+    using t2 = (操作2 で作用させる値の型);
+    static t1 id1() { t1 の単位元; }
+    static t2 id2() { t2 の単位元; }
+    static t1 op1(const t1 &l, const t1 &r) { return (型1同士の連結に用いる演算)(l, r); }
+    static t1 op2(const t1 &l, const t2 &r) { return (操作2 で型2 を作用させる演算)(l, r); }
+    static t2 op3(const t2 &l, const t2 &r) { return (型2 同士の演算); }
+};
+ */
+struct RangeAddRangeSum
+{
+    using t1 = long long;
+    using t2 = long long;
+    static t1 id1() { return 0; }
+    static t2 id2() { return 0; }
+    static t1 op1(const t1 &l, const t1 &r) { return l + r; }
+    static t1 op2(const t1 &l, const t2 &r) { return l + r; }
+    static t2 op3(const t2 &l, const t2 &r) { return l + r; }
+};
+/*
+struct RamgeUpdateRangeMinimum
+{
+    using t1 = int;
+    using t2 = int;
+    static t1 id1() { return INT_MAX; }
+    static t2 id2() { return -1; }
+    static t1 op1(const t1 &l, const t1 &r) { return std::min(l, r); }
+    static t1 op2(const t1 &l, const t2 &r) { return r != id2() ? r : l; }
+    static t2 op3(const t2 &l, const t2 &r) { return r != id2() ? r : l; }
+};
+*/
--- Structure/recursive_lazy_segment_tree.hpp
+++ Structure/recursive_lazy_segment_tree.hpp
@@ -3,6 +3,7 @@
 // 区間加算区間和 再帰遅延セグ木 0-indexed
 // TODO: 非再帰, template
 // verified: http://judge.u-aizu.ac.jp/onlinejudge/review.jsp?rid=3380844
+template <typename M>
 class LazySegmentTree
 {
     int n;
@@ -12,18 +13,18 @@ class LazySegmentTree
     // k-th element 遅延評価
     void eval(int k, int l, int r)
     {
-        if (lazy[k])
+        if (lazy[k] != M::id2())
         {
-            node[k] += lazy[k];
+            node[k] = M::op2(node[k], lazy[k]);
 
             // 葉ノードでなければ伝播
             if (r - l > 1)
             {
-                lazy[2 * k + 1] += lazy[k] / 2;
-                lazy[2 * k + 2] += lazy[k] / 2;
+                lazy[2 * k + 1] = M::op3(lazy[2 * k + 1], lazy[k] / 2);
+                lazy[2 * k + 2] = M::op3(lazy[2 * k + 2], lazy[k] / 2);
             }
 
-            lazy[k] = 0;
+            lazy[k] = M::id2();
         }
     }
 
@@ -35,15 +36,15 @@ class LazySegmentTree
         while (n < sz)
             n *= 2;
         node.resize(2 * n - 1);
-        lazy.resize(2 * n - 1, 0);
+        lazy.resize(2 * n - 1, M::id2());
 
         for (int i = 0; i < sz; i++)
             node[i + n - 1] = v[i];
         for (int i = n - 2; i >= 0; i--)
-            node[i] = node[i * 2 + 1] + node[i * 2 + 2];
+            node[i] = M::op1(node[i * 2 + 1], node[i * 2 + 2]);
     }
 
-    // 区間add
+    // 区間 op2
     void add(int a, int b, type x, int k = 0, int l = 0, int r = -1)
     {
         if (r < 0)
@@ -56,6 +57,7 @@ class LazySegmentTree
 
         if (a <= l && r <= b)
         {
+            // ここ困る 区間add 用
             lazy[k] += (r - l) * x;
             eval(k, l, r);
         }
@@ -63,7 +65,7 @@ class LazySegmentTree
         {
             add(a, b, x, 2 * k + 1, l, (l + r) / 2);
             add(a, b, x, 2 * k + 2, (l + r) / 2, r);
-            node[k] = node[2 * k + 1] + node[2 * k + 2];
+            node[k] = M::op1(node[2 * k + 1], node[2 * k + 2]);
         }
     }
 
@@ -73,13 +75,13 @@ class LazySegmentTree
         if (r < 0)
             r = n;
         if (b <= l || r <= a)
-            return 0;
+            return M::id1();
 
         eval(k, l, r);
         if (a <= l && r <= b)
             return node[k];
         type vl = sum(a, b, 2 * k + 1, l, (l + r) / 2);
         type vr = sum(a, b, 2 * k + 2, (l + r) / 2, r);
-        return vl + vr;
+        return M::op1(vl, vr);
     }
 };
