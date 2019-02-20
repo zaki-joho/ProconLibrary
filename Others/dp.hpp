487397d12fcd622abd95dbee6a5c7e6397263148
zaki-joho <zaki.joho.ku@gmail.com>
2019-02-21 00:15:20 +0900

    fix dp

--- Others/dp.hpp
+++ Others/dp.hpp
@@ -13,4 +13,38 @@ int lis(std::vector<int> &a, int n)
     return lower_bound(dp.begin(), dp.end(), INF) - dp.begin();
 }
 
-// 長方形面積
+/**
+ * ヒストグラム h_1, h_2,..., h_n が与えられる(各幅は1とする)
+ * 包含される長方形の内面積最大のものを求める
+ * L[i] := (j <= i && h_{j-1} < h_i) となるような最大の j
+ * R[i] := (j > i && h_j < h_i) となるような最大の j
+ */
+template <typename T>
+T maxRectangle(std::vector<T> &h)
+{
+    int n = h.size();
+    std::vector<int> L(n), R(n);
+    std::vector<int> st(n);
+
+    int t = 0; // stack size
+    for (int i = 0; i < n; i++)
+    {
+        while (t > 0 && h[st[t - 1]] >= h[i])
+            t--;
+        L[i] = t == 0 ? 0 : (st[t - 1] + 1);
+        st[t++] = i;
+    }
+    t = 0;
+    for (int i = n - 1; i >= 0; i--)
+    {
+        while (t > 0 && h[st[t - 1]] >= h[i])
+            t--;
+        R[i] = t == 0 ? n : st[t - 1];
+        st[t++] = i;
+    }
+
+    T ret = 0;
+    for (int i = 0; i < n; i++)
+        ret = std::max(ret, h[i] * (R[i] - L[i]));
+    return ret;
+}
