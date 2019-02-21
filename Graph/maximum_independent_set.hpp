d827821346c0e66c8b318a8a50ec10fc6c23f1ee
zaki-joho <zaki.joho.ku@gmail.com>
2019-02-21 12:13:08 +0900

    fix maximum independent set

--- Graph/maximum_independent_set.hpp
+++ Graph/maximum_independent_set.hpp
@@ -3,27 +3,21 @@
 // 最小頂点被覆問題と等価
 // 補グラフの最大クリーク問題と等価
 // 2部グラフ上の最大独立集合問題 = 頂点数 - 最大マッチング
-/*
-int popcount(unsigned long long a) {
-    a = (a & 0x5555555555555555) + (a >> 1 & 0x5555555555555555);
-    a = (a & 0x3333333333333333) + (a >> 2 & 0x3333333333333333);
-    a = (a & 0x0f0f0f0f0f0f0f0f) + (a >> 4 & 0x0f0f0f0f0f0f0f0f);
-    a = (a & 0x00ff00ff00ff00ff) + (a >> 8 & 0x00ff00ff00ff00ff);
-    a = (a & 0x0000ffff0000ffff) + (a >> 16 & 0x0000ffff0000ffff);
-    return (a & 0xffffffff) + (a >> 32);
-}
-
-int maximumIndependentSet(const std::vector<long long> &g)
+// verified: https://codeforces.com/contest/1105/submission/50254980
+class MaximumIndependentSet
 {
-    int n = g.size(), res = 0;
-    std::function<void(long long, int)> dfs = [&](long long remain, int cnt) {
+    int n, ret = 0;
+    std::vector<long long> g;
+
+    int dfs(long long remain, int cnt)
+    {
         for (bool update = true; update;)
         {
             update = false;
             for (int i = 0; i < n; i++)
                 if (remain & (1LL << i))
                 {
-                    int deg = popcount(remain & g[i]);
+                    int deg = __builtin_popcountll(remain & g[i]);
                     if (deg <= 1)
                     {
                         cnt++;
@@ -32,15 +26,25 @@ int maximumIndependentSet(const std::vector<long long> &g)
                     }
                 }
         }
-        res = std::max(res, cnt);
+        ret = std::max(ret, cnt);
         if (remain)
         {
             int k = __builtin_ctzll(remain);
             dfs(remain & ~(1LL << k), cnt);
             dfs(remain & ~(g[k] | (1LL << k)), cnt + 1);
         }
-    };
-    dfs((1LL << n) - 1, 0);
-    return res;
-}
-*/
+    }
+
+  public:
+    MaximumIndependentSet(int _n) : n(_n), g(n, 0) {}
+    void addEdge(int u, int v)
+    {
+        g[u] |= (1LL << v);
+        g[v] |= (1LL << u);
+    }
+    int solve()
+    {
+        dfs((1LL << n) - 1, 0);
+        return ret;
+    }
+};
