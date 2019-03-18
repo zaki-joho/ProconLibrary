4e95c356854201ff38c32e9836ab433d8036451c
zaki-joho <zaki.joho.ku@gmail.com>
2019-03-19 02:24:27 +0900

    add non recursive segment tree

--- Structure/recursive_segment_tree.hpp
+++ Structure/recursive_segment_tree.hpp
@@ -6,13 +6,13 @@
 template <typename M>
 class SegmentTree
 {
+    using T = typename M::type;
     int n;
-    const int INF = 2147483647;
-    std::vector<int> node;
+    std::vector<T> node;
 
   public:
     // v を基に初期化
-    SegmentTree(const std::vector<int> &v)
+    SegmentTree(const std::vector<T> &v)
     {
         // n は v.size() 以上の最小の2冪
         n = 1;
@@ -26,8 +26,17 @@ class SegmentTree
         for (int i = n - 2; i >= 0; i--)
             node[i] = M::op(node[2 * i + 1], node[2 * i + 2]);
     }
+    // Monoid::id 初期化
+    SegmentTree(int _n)
+    {
+        // n は v.size() 以上の最小の2冪
+        n = 1;
+        while (n < _n)
+            n *= 2;
+        node.resize(2 * n - 1, M::id());
+    }
     // x 番目を val に更新
-    void update(int x, int val)
+    void update(int x, T val)
     {
         x += n - 1;
         node[x] = val;
@@ -38,7 +47,7 @@ class SegmentTree
         }
     }
     // v[x] を M::op(v[x], val) に更新
-    void add(int x, int val)
+    void add(int x, T val)
     {
         x += n - 1;
         node[x] = M::op(node[x], val);
@@ -50,7 +59,7 @@ class SegmentTree
     }
     // [a, b) の op
     // k := 今居るノード
-    int query(int a, int b, int k = 0, int l = 0, int r = -1)
+    T query(int a, int b, int k = 0, int l = 0, int r = -1)
     {
         if (r < 0)
             r = n;
@@ -59,8 +68,8 @@ class SegmentTree
         if (a <= l && r <= b)
             return node[k];
 
-        int vl = query(a, b, 2 * k + 1, l, (l + r) / 2);
-        int vr = query(a, b, 2 * k + 2, (l + r) / 2, r);
+        T vl = query(a, b, 2 * k + 1, l, (l + r) / 2);
+        T vr = query(a, b, 2 * k + 2, (l + r) / 2, r);
         return M::op(vl, vr);
     }
 };
