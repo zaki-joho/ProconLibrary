36d6c9698bf582caaf65f7afd1935c4764cafc49
zaki-joho <zaki.joho.ku@gmail.com>
2019-03-21 18:28:15 +0900

    add lazy binary trie

--- Structure/lazy_binary_trie.hpp
+++ Structure/lazy_binary_trie.hpp
@@ -1,7 +1,6 @@
 #include "../bits/stdc++.h"
-// http://kazuma8128.hatenablog.com/entry/2018/05/06/022654
-// verified(insert, erase, k-th element): https://atcoder.jp/contests/arc033/submissions/4648138
-// verified(xor minimum): https://codeforces.com/contest/947/submission/51609141
+// binary trie に対し, 全要素にある値を xor する操作を追加したもの
+// verifie してない: https://codeforces.com/contest/966/problem/C
 class BinaryTrie
 {
     using T = unsigned;
@@ -11,14 +10,27 @@ class BinaryTrie
     {
         // 要素数
         int cnt;
+        // xor する値
+        T lazy;
         Node *ch[2];
-        Node() : cnt(0)
+        Node() : cnt(0), lazy(0)
         {
             ch[0] = nullptr, ch[1] = nullptr;
         }
     };
 
     Node *root;
+
+    void eval(Node *t, int b)
+    {
+        if ((t->lazy >> (T)b) & (T)1)
+            std::swap(t->ch[0], t->ch[1]);
+        if (t->ch[0])
+            t->ch[0]->lazy ^= t->lazy;
+        if (t->ch[1])
+            t->ch[1]->lazy ^= t->lazy;
+        t->lazy = 0;
+    }
 
     // Node t 以下に 値 val を追加
     Node *add(Node *t, T val, int b = B - 1)
@@ -28,6 +40,7 @@ class BinaryTrie
         t->cnt += 1;
         if (b < 0)
             return t;
+        eval(t, b);
         bool f = (val >> (T)b) & (T)1;
         t->ch[f] = add(t->ch[f], val, b - 1);
         return t;
@@ -42,34 +55,38 @@ class BinaryTrie
             return nullptr;
         if (b < 0)
             return t;
+        eval(t, b);
         bool f = (val >> (T)b) & (T)1;
         t->ch[f] = sub(t->ch[f], val, b - 1);
         return t;
     }
     // Node t 以下の k-th element を取得
-    T get(Node *t, int k, int b = B - 1) const
+    T get(Node *t, int k, int b = B - 1)
     {
         assert(t);
         if (b < 0)
             return 0;
+        eval(t, b);
         int m = t->ch[0] ? t->ch[0]->cnt : 0;
         return k < m ? get(t->ch[0], k, b - 1) : get(t->ch[1], k - m, b - 1) | ((T)1 << (T)b);
     }
     // Node t 以下で val と xor するときの最小値取得
-    T getMinimum(Node *t, T val, int b = B - 1) const
+    T getMinimum(Node *t, T val, int b = B - 1)
     {
         assert(t);
         if (b < 0)
             return 0;
+        eval(t, b);
         bool f = (val >> (T)b) & (T)1;
         f ^= !t->ch[f];
         return getMinimum(t->ch[f], val, b - 1) | ((T)f << (T)b);
     }
     // Node t 以下の val 未満の要素数
-    int countLessThan(Node *t, T val, int b = B - 1) const
+    int countLessThan(Node *t, T val, int b = B - 1)
     {
         if (!t || b < 0)
             return 0;
+        eval(t, b);
         bool f = (val >> (T)b) & (T)1;
         return (f && t->ch[0] ? t->ch[0]->cnt : 0) + countLessThan(t->ch[f], val, b - 1);
     }
@@ -95,43 +112,50 @@ class BinaryTrie
         root = sub(root, val);
     }
     // 集合内の最大値
-    T maximumElement(T mask = 0) const
+    T maximumElement(T mask = 0)
     {
         return getMinimum(root, ~mask);
     }
     // 集合内の最小値
-    T minimumElement(T mask = 0) const
+    T minimumElement(T mask = 0)
     {
         return getMinimum(root, mask);
     }
     // 集合内で val 以上の要素の最小index
-    int lowerBound(T val) const
+    int lowerBound(T val)
     {
         return countLessThan(root, val);
     }
     // 集合内で val より大きい要素の最小index
-    int upperBound(T val) const
+    int upperBound(T val)
     {
         return countLessThan(root, val + 1);
     }
     // k-th element(0-indexed) を取得
-    T get(int k) const
+    T get(int k)
     {
         assert(0 <= k && k < size());
         return get(root, k);
     }
     // val の個数
-    int count(T val) const
+    int count(T val)
     {
         if (!root)
             return 0;
         Node *t = root;
         for (int i = B - 1; i >= 0; i--)
         {
+            eval(t, i);
             t = t->ch[(val >> (T)i) & (T)1];
             if (!t)
                 return 0;
         }
         return t->cnt;
+    }
+    // 全ての要素を val で xor した値に変更
+    void xorAllElements(T val)
+    {
+        if (root)
+            root->lazy ^= val;
     }
 };
