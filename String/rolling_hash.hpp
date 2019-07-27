ab6697fb36e12815042d2c6d0084d57c1fa6cf17
zaki-joho <zaki.joho.ku@gmail.com>
2019-07-28 01:18:21 +0900

    update rolling hash

--- String/rolling_hash.hpp
+++ String/rolling_hash.hpp
@@ -1,16 +1,19 @@
 #include "../bits/stdc++.h"
+#include "../Others/xor_shift.hpp"
 
 // verified: http://judge.u-aizu.ac.jp/onlinejudge/review.jsp?rid=3381829
 // moda, modb は桁数増やした方が良いかも.
 class RollingHash
 {
     int n;
+    XorShift rnd;
     static const uint64_t moda = (uint64_t)1e9 + 7;
     static const uint64_t modb = (uint64_t)1e9 + 9;
-    static const uint64_t b1 = (uint64_t)17, b2 = (uint64_t)19;
+    // static const uint64_t b1 = (uint64_t)17, b2 = (uint64_t)19;
+    uint64_t b1 = (uint64_t)rnd.randInt(2, 114514), b2 = (uint64_t)rnd.randInt(2, 114514);
     std::vector<uint64_t> a, b, p, q;
 
-  public:
+public:
     RollingHash(const std::string &str) : n((int)str.size()), a(n + 1, 0), b(n + 1, 0), p(n + 1, 1), q(n + 1, 1)
     {
         for (int i = 0; i < n; i++)
