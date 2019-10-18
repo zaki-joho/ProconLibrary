ce157889df138150382bc7c23e88f7cee115fc44
Eto Nagisa <taiga.tiger.tora@gmail.com>
2019-10-18 21:37:48 +0900

    add buyild script

---
+++ ICPC/script/compile.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+for file in `find|grep .hpp`
+do
+    echo compiling $file
+    if ! g++ -std=c++14 -c $file -o /dev/null ; then
+        echo compile error on $file
+        exit 1
+    fi
+    echo ok
+done
