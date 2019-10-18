c613a7267a603606eb356e533fbdac245b5d2f23
nikutto <nikuto@outlook.com>
2019-10-18 12:29:40 +0900

    renew cost_type

--- ICPC/Graph/graph.hpp
+++ ICPC/Graph/graph.hpp
@@ -1,8 +1,9 @@
 #include<vector>
 
+using cost_t=int;
 struct Edge {
     int from, to, cost;
-    Edge(int from, int to, int cost) : from(from), to(to), cost(cost) {}
+    Edge(int from, int to, cost_t cost) : from(from), to(to), cost(cost) {}
 };
 
 using Edges = std::vector<Edge>;
