f12106cdd45db1c7d685567ff0fdad83587ec2b0
zaki-joho <zaki.joho.ku@gmail.com>
2019-10-21 18:29:30 +0900

    add structure hpp

---
+++ ICPC/Structure/fenwick_tree.hpp
@@ -0,0 +1,35 @@
+#include "../bits/stdc++.h"
+
+template <typename T>
+class FenwickTree
+{
+    int n;
+    std::vector<T> bt;
+
+public:
+    FenwickTree(int _n) : n(_n), bt(_n, 0) {}
+
+    void add(int pos, const T &w)
+    {
+        assert(0 <= pos && pos < n);
+        for (int i = pos; i < n; i |= i + 1)
+            bt[i] += w;
+    }
+    // [0, pos)
+    T sum(int pos)
+    {
+        assert(0 <= pos && pos <= n);
+        T res = 0;
+        for (int i = pos - 1; i >= 0; i = (i & (i + 1)) - 1)
+        {
+            res += bt[i];
+        }
+        return res;
+    }
+    // [l, r)
+    T sum(int l, int r)
+    {
+        assert(0 <= l && l <= r && r <= n);
+        return sum(r) + (-sum(l));
+    }
+};
