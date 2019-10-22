0148adb077c0404bf4ed333da763dac808fa8e7e
zaki-joho <zaki.joho.ku@gmail.com>
2019-10-22 22:05:32 +0900

    update HLD test

--- ICPC/Test/heavy_light_decomposition_test.cpp
+++ ICPC/Test/heavy_light_decomposition_test.cpp
@@ -94,9 +94,49 @@ void solve_grl_5_e()
     }
 }
 
-// 頂点クエリテストしてない
+// verified: https://yukicoder.me/submissions/392372
+void solve_yukicoder_399()
+{
+    using ll = long long;
+    int n;
+    cin >> n;
+    HLDecomposition hld(n);
+    LazySegmentTree lst(n);
+    for (int i = 0; i < n - 1; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        u--;
+        v--;
+        hld.add_edge(u, v);
+    }
+    hld.build();
+    for (int i = 0; i < n; i++)
+    {
+        hld.for_each_vertex(i, i, [&](int l, int r) {
+            lst.update(l, r + 1, 1);
+        });
+    }
+    int q;
+    cin >> q;
+    ll ret = 0;
+    while (q--)
+    {
+        int a, b;
+        cin >> a >> b;
+        a--;
+        b--;
+        hld.for_each_vertex(a, b, [&](int l, int r) {
+            ret += lst.query(l, r + 1);
+        });
+        hld.for_each_vertex(a, b, [&](int l, int r) {
+            lst.update(l, r + 1, 1);
+        });
+    }
+    cout << ret << endl;
+}
 
 int main()
 {
-    solve_grl_5_d();
+    solve_yukicoder_399();
 }
