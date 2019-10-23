297503d0f74a2f04b24daa935f64d5c28b5de16a
zaki-joho <zaki.joho.ku@gmail.com>
2019-10-24 00:08:04 +0900

    update CHT test

--- ICPC/Test/convex_hull_trick_test.cpp
+++ ICPC/Test/convex_hull_trick_test.cpp
@@ -2,100 +2,121 @@
 
 using namespace std;
 
-class LiChaoTree {
-	using T = long long;
-	using Line = pair<T, T>;
-	const T inf = 1e15;
-	// 最小化?
-	bool isMinimum;
-	int n;
-	// 各区間には, その区間で最小(or最大)を取りうる直線を保持する
-	vector<Line> node;
-
-	static inline T f(const Line &line, T x) {return line.first * x + line.second;}
-
-	// [l, r) に Line を追加, 現在ノードk
-	void add(int k, int l, int r, Line line)
+using CHT_TYPE = long long;
+class ConvexHullTrickDynamic
+{
+private:
+	// 直線 **************************************************************
+	struct Line
 	{
-		while(r-l>0)
+		CHT_TYPE a, b;								  // y = ax + b
+		mutable std::function<const Line *()> getSuc; // 次の直線へのポインタ (ソートで用いる)
+
+		bool operator<(const Line &rhs) const
 		{
-			if(node[k]==Line(0, -inf))
+			// 取得クエリでは次の直線との差分でソート
+			if (rhs.b == IS_QUERY)
 			{
-				node[k] = line;
-				return;
+				const Line *suc = getSuc();
+				if (suc == nullptr)
+					return false;
+				const CHT_TYPE &x = rhs.a;
+				return (suc->a - a) * x + suc->b - b > 0;
 			}
-			int m = (l + r) / 2;
-			bool left = !(f(line, l) < f(node[k], l));
-			bool mid = !(f(line, m) < f(node[k], m));
-			bool right = !(f(line, r) < f(node[k], r));
-			if(left && right)
+			if (b == IS_QUERY)
 			{
-				node[k] = line;
-				return;
+				const Line *suc = rhs.getSuc();
+				if (suc == nullptr)
+					return true;
+				const CHT_TYPE &x = a;
+				return (suc->a - rhs.a) * x + suc->b - rhs.b < 0;
 			}
-			if(!left && !right)
-			{
-				return;
-			}
-			if(mid)
-			{
-				swap(line, node[k]);
-			}
-			if(left != mid)
-			{
-				k = 2 * k + 1;
-				r = m;
+
+			// 通常の直線どうしは傾きソート
+			return a < rhs.a;
+		}
+	};
+
+	// 直線集合 **********************************************************
+	class LinesSet : public std::multiset<Line>
+	{
+	private:
+		// true -> 最小値クエリ, false -> 最大値クエリ
+		bool flagMin;
+
+	public:
+		// コンストラクタ ( 第一引数falseで最大値クエリ,デフォルトで最小値クエリ )
+		LinesSet(bool flagMin = false) : flagMin(flagMin){};
+
+		// 直線lが不必要であるかどうか
+		inline bool isBad(iterator l)
+		{
+			const auto &&nel = std::next(l);
+			if (l == begin())
+			{ // lが傾き最小のとき
+				if (nel == end())
+					return false; // lしかないなら必要
+				return l->a == nel->a && l->b <= nel->b;
 			}
 			else
 			{
-				k = 2 * k + 1;
-				l = m;
+				const auto &&prl = std::prev(l);
+				if (nel == end())
+					return l->a == prl->a && l->b <= prl->b;
+				return (prl->b - l->b) * (nel->a - l->a) >= (nel->b - l->b) * (prl->a - l->a);
 			}
 		}
-	}
+
+		// 直線y=ax+bを追加する
+		inline void add(CHT_TYPE a, CHT_TYPE b)
+		{
+			if (flagMin)
+				a = -a, b = -b;
+			auto &&it = insert({a, b});
+			it->getSuc = [=] { return (std::next(it) == end() ? nullptr : &*std::next(it)); };
+			if (isBad(it))
+			{
+				erase(it);
+				return;
+			}
+			while (std::next(it) != end() && isBad(std::next(it)))
+				erase(std::next(it));
+			while (it != begin() && isBad(std::prev(it)))
+				erase(std::prev(it));
+		}
+
+		// 直線群の中でxの時に最小(最大)となる値を返す
+		inline CHT_TYPE get(CHT_TYPE x)
+		{
+			auto &&l = *lower_bound(Line{x, IS_QUERY});
+			if (flagMin)
+				return -l.a * x - l.b;
+			else
+				return l.a * x + l.b;
+		}
+	};
+
+	static const CHT_TYPE IS_QUERY = std::numeric_limits<CHT_TYPE>::lowest();
+	LinesSet linesSet;
 
 public:
-	// [0, n] 用の木を作る
-	LiChaoTree(int _n, bool isMin=false) : isMinimum(isMin) {
-		n = 1;
-		while(n < _n) n *= 2;
-		node.resize(2 * n - 1, Line(0, isMin ? inf : -inf));
+	// コンストラクタ ( 第一引数falseで最大値クエリ,デフォルトで最小値クエリ )
+	ConvexHullTrickDynamic(bool flagMin = false) : linesSet(flagMin) {
+		linesSet.add(0, -1e15);
 	}
-	//　ax + b を追加する
-	void add(T a, T b)
-	{
-		add(0, 0, n, Line(a, b));
-		return;
-	}
-	T get(T x)
-	{
-		int k = x + (n - 1);
-		T ret = (node[k]!=Line(0, -inf) ? f(node[k], x) : -inf);
-		while(k > 0)
-		{
-			k = (k-1)/2;
-			if(node[k]!=Line(0, -inf))
-			{
-				ret = max(ret, f(node[k], x));
-			}
-		}
-		return ret;
-	}
+	// 直線y=ax+bを追加する
+	inline void add(CHT_TYPE a, CHT_TYPE b) { linesSet.add(a, b); }
+	// あるxのときの直線集合での最小値を求める
+	inline CHT_TYPE get(CHT_TYPE x) { return linesSet.get(x); }
 };
 
-using ll = long long;
-const ll inf = 1e15;
-
-// dp[i][j] := i曲目まで歌って, 時間j 消費した
-ll dp[4000 + 1][4000 + 1];
-vector<LiChaoTree> cht(4000 + 1, LiChaoTree(4000));
-
-// http://judge.u-aizu.ac.jp/onlinejudge/review.jsp?rid=3946763
+// http://judge.u-aizu.ac.jp/onlinejudge/review.jsp?rid=3947471
 void solve_aoj_2725()
 {
+	using ll = long long;
+	const ll inf = 1e15;
 	int N, TMAX;
 	cin >> N >> TMAX;
-	for(int i=0;i<=N;i++)for(int j=0;j<=TMAX;j++) dp[i][j]=-inf;
 	struct Song
 	{
 		ll t, p, f;
@@ -108,6 +129,9 @@ void solve_aoj_2725()
 		songs.push_back(Song{t, p, f});
 	}
 	sort(songs.begin(), songs.end(), [](const Song &e, const Song &f) { return e.f < f.f; });
+	// dp[i][j] := i曲目まで歌って, 時間j 消費した
+	vector<vector<ll>> dp(N + 1, vector<ll>(TMAX + 1, -inf));
+	vector<ConvexHullTrickDynamic> cht(TMAX + 1);
 	ll ret = 0;
 	dp[0][0] = 0;
 	for (int i = 0; i < N; i++)
