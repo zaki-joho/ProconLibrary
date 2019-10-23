d9d8d057d1d0bdd5aa2234e4d5c61b73f67e0a81
zaki-joho <zaki.joho.ku@gmail.com>
2019-10-23 20:36:50 +0900

    add lichao tree

--- ICPC/Test/convex_hull_trick_test.cpp
+++ ICPC/Test/convex_hull_trick_test.cpp
@@ -3,7 +3,87 @@
 using namespace std;
 
 class LiChaoTree {
-	LiChaoTree() {}
+	using T = long long;
+	using Line = pair<T, T>;
+	const T inf = 1e9;
+	// 最小化?
+	bool isMinimum;
+	int n;
+	// 各区間には, その区間で最小(or最大)を取りうる直線を保持する
+	vector<Line> node;
+	vector<int> usd;
+
+	static inline T f(const Line &line, T x) {return line.first * x + line.second;}
+
+	// [l, r) に Line を追加, 現在ノードk
+	void add(int k, int l, int r, Line line)
+	{
+		while(r-l>0)
+		{
+			if(!usd[k])
+			{
+				node[k] = line;
+				usd[k] = true;
+				return;
+			}
+			int m = (l + r) / 2;
+			bool left = f(line, l) < f(node[k], l);
+			bool mid = f(line, m) < f(node[k], m);
+			bool right = f(line, r) < f(node[k], r);
+			if(left && right)
+			{
+				node[k] = line;
+				return;
+			}
+			if(!left && !right)
+			{
+				return;
+			}
+			if(mid)
+			{
+				swap(line, node[k]);
+			}
+			if(left != mid)
+			{
+				k = 2 * k + 1;
+				r = m;
+			}
+			else
+			{
+				k = 2 * k + 1;
+				l = m;
+			}
+		}
+	}
+
+public:
+	// [0, n] 用の木を作る
+	LiChaoTree(int _n, bool isMin=true) : isMinimum(isMin) {
+		n = 1;
+		while(n < _n) n *= 2;
+		node.resize(2 * n - 1, Line(0, isMin ? inf : -inf));
+		usd.resize(2 * n -1, false);
+	}
+	//　ax + b を追加する
+	void add(T a, T b)
+	{
+		add(0, 0, n, Line(a, b));
+		return;
+	}
+	T get(T x)
+	{
+		int k = x + (n - 1);
+		T ret = usd[k] ? f(node[k], x) : inf;
+		while(k > 0)
+		{
+			k = (k-1)/2;
+			if(usd[k])
+			{
+				ret = min(ret, f(node[k], x));
+			}
+		}
+		return ret;
+	}
 };
 
 // http://judge.u-aizu.ac.jp/onlinejudge/review.jsp?rid=3946763
@@ -27,7 +107,7 @@ void solve_aoj_2725()
 	sort(songs.begin(), songs.end(), [](const Song &e, const Song &f) { return e.f < f.f; });
 	// dp[i][j] := i曲目まで歌って, 時間j 消費した
 	vector<vector<ll>> dp(N + 1, vector<ll>(TMAX + 1, -inf));
-	vector<LiChaoTree> cht(TMAX + 1);
+	vector<LiChaoTree> cht(TMAX + 1, LiChaoTree(4000));
 	ll ret = 0;
 	dp[0][0] = 0;
 	for (int i = 0; i < N; i++)
