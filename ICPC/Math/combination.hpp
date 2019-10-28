d1b5f0f3adc0ab2a618a93891c60356d61bf1d91
nikutto <nikuto@outlook.com>
2019-10-28 15:40:21 +0900

    Add tex index and modify combination

--- ICPC/Math/combination.hpp
+++ ICPC/Math/combination.hpp
@@ -1,14 +1,12 @@
-#include "../bits/stdc++.h"
-constexpr int MOD = 1e9 + 7;
 
-// a^b O(log b)
-template<typename T>
-T mypow(T a, T b) {
-    if (b == 0)
-        return 1;
-    T tmp = mypow(a, b / 2);
-    if (b % 2)
-        return (((tmp * tmp) % MOD) * a) % MOD;
-    else
-        return (tmp * tmp) % MOD;
+using ll=long long;
+
+ll powm(ll x,ll k,ll mod){
+    ll res=1;
+    while(k){
+        if(k&1) res=res*x%MOD;
+        x=x*x%MOD;
+        k>>=1;
+    }
+    return res;
 }
