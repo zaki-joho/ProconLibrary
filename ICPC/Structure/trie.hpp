dda164af1bab0a72b2bc4524bfe71ff7ea09df01
zaki-joho <zaki.joho.ku@gmail.com>
2019-10-28 23:16:53 +0900

    format structure

--- ICPC/Structure/trie.hpp
+++ ICPC/Structure/trie.hpp
@@ -1,32 +1,25 @@
 #include "../bits/stdc++.h"
 
-struct Trie
-{
-    Trie *node[26];
-    int val;
-    Trie()
-    {
-        val = 0;
-        std::fill(node, node + 26, (Trie *)0);
+struct Trie {
+  Trie *node[26];
+  int val;
+  Trie() {
+    val = 0;
+    std::fill(node, node + 26, (Trie *)0);
+  }
+  void insert(const std::string &s, int w) {
+    Trie *r = this;
+    for (int i = 0; i < (int)s.length(); i++) {
+      int c = s[i] - 'a';
+      if (!r->node[c]) r->node[c] = new Trie;
+      r = r->node[c];
     }
-    void insert(const std::string &s, int w)
-    {
-        Trie *r = this;
-        for (int i = 0; i < (int)s.length(); i++)
-        {
-            int c = s[i] - 'a';
-            if (!r->node[c])
-                r->node[c] = new Trie;
-            r = r->node[c];
-        }
-        r->val = w;
-        return;
-    }
-    Trie *find(const char &s, Trie *pos)
-    {
-        int c = s - 'a';
-        if (!pos->node[c])
-            return (Trie *)0;
-        return pos->node[c];
-    }
+    r->val = w;
+    return;
+  }
+  Trie *find(const char &s, Trie *pos) {
+    int c = s - 'a';
+    if (!pos->node[c]) return (Trie *)0;
+    return pos->node[c];
+  }
 };
--- ICPC/Structure/union_find.hpp
+++ ICPC/Structure/union_find.hpp
@@ -1,40 +1,24 @@
 #include "../bits/stdc++.h"
 
-class UnionFind
-{
-    std::vector<int> data;
-    int root_count; // 集合の個数
+class UnionFind {
+  std::vector<int> data;
+  int root_count;  // 集合の個数
 
-public:
-    UnionFind(int size) : data(size, -1), root_count(size) {}
-    bool unite(int x, int y)
-    {
-        x = root(x);
-        y = root(y);
-        if (x != y)
-        {
-            if (data[y] < data[x])
-                std::swap(x, y);
-            data[x] += data[y];
-            data[y] = x;
-            root_count--;
-        }
-        return x != y;
+ public:
+  UnionFind(int size) : data(size, -1), root_count(size) {}
+  bool unite(int x, int y) {
+    x = root(x);
+    y = root(y);
+    if (x != y) {
+      if (data[y] < data[x]) std::swap(x, y);
+      data[x] += data[y];
+      data[y] = x;
+      root_count--;
     }
-    bool find(int x, int y)
-    {
-        return root(x) == root(y);
-    }
-    int root(int x)
-    {
-        return data[x] < 0 ? x : data[x] = root(data[x]);
-    }
-    int size(int x)
-    {
-        return -data[root(x)];
-    }
-    int count() const
-    {
-        return root_count;
-    }
+    return x != y;
+  }
+  bool find(int x, int y) { return root(x) == root(y); }
+  int root(int x) { return data[x] < 0 ? x : data[x] = root(data[x]); }
+  int size(int x) { return -data[root(x)]; }
+  int count() const { return root_count; }
 };
