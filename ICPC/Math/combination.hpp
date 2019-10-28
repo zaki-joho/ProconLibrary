e53e473e49b29238893543fdeb802a181d55e55b
nikutto <nikuto@outlook.com>
2019-10-28 15:45:52 +0900

    fix combination

--- ICPC/Math/combination.hpp
+++ ICPC/Math/combination.hpp
@@ -4,8 +4,8 @@ using ll=long long;
 ll powm(ll x,ll k,ll mod){
     ll res=1;
     while(k){
-        if(k&1) res=res*x%MOD;
-        x=x*x%MOD;
+        if(k&1) res=res*x%mod;
+        x=x*x%mod;
         k>>=1;
     }
     return res;
---
+++ ICPC/Math/garnerCRT.hpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+using ll=long long;
+const ll MOD=1e9+7;
+// all m must be coprime each other.
+// O(K^2), where K is size of b and m
+// return 
+ll crt(vector<ll> b,vector<ll> m){
+    for(int i=0;i<)
+    
+}
