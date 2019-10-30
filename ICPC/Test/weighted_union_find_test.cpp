3a4490bc05b2e08fa6c9ab356fceaf6c433b2933
zaki-joho <zaki.joho.ku@gmail.com>
2019-10-30 13:42:07 +0900

    add wuf

---
+++ ICPC/Test/weighted_union_find_test.cpp
@@ -0,0 +1,27 @@
+#include "../Structure/weighted_union_find.hpp"
+#include "../bits/stdc++.h"
+
+using namespace std;
+
+int main() {
+  int n, q;
+  cin >> n >> q;
+  WeightedUnionFind uf(n);
+  for (int i = 0; i < q; i++) {
+    int id;
+    cin >> id;
+    if (id) {
+      int x, y;
+      cin >> x >> y;
+      if (uf.same(x, y)) {
+        cout << -uf.weight(x) + uf.weight(y) << endl;
+      } else {
+        cout << "?" << endl;
+      }
+    } else {
+      int x, y, z;
+      cin >> x >> y >> z;
+      uf.unite(x, y, z);
+    }
+  }
+}
